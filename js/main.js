882f40d726e8d3a909abca8a14554e6e79bcd432
muglatarim <ozcanibrahim@tarimorman.gov.tr>
2026-03-19 11:54:04 +0300

    iboo48.github.io/desteklemeler

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@ function initTabs() {
       if (target === 'destekleme' && !desteklemeLoaded) {
         desteklemeLoaded = true;
         const frame = document.getElementById('destekleme-frame');
-        if (frame) frame.src = 'https://muglatarim.github.io/desteklemeler/';
+        if (frame) frame.src = 'https://iboo48.github.io/desteklemeler/';
       }
 
       // Tabları güncelle
